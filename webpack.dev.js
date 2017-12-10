e9577e2d63b4e34ee838673cd4e477735a7a277f
Alex Thorne <alex@thorne.cc>
2017-12-10 17:37:10 +0000

    Split webpack config into separate dev and prod
    Add appropriate source mapping tools to each.

---
+++ webpack.dev.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  devtool: 'cheap-module-eval-source-map'
+})
