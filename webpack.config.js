4b3ba0b60042a28c241cc4ea54e4ed02e22e197a
Alex Thorne <alex@thorne.cc>
2017-12-10 17:13:00 +0000

    Configure ES6+ transpilation with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,16 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
       { test: /\.(jpg|png|gif|svg|tiff)$/, use: 'file-loader' },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' }
