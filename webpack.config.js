696bc2c4dbc1ea07904dc7606e371601ac0490a4
Alex Thorne <alex@thorne.cc>
2017-12-10 17:02:11 +0000

    Configure webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(jpg|png|gif|svg|tiff)$/, use: 'file-loader' },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' }
+    ]
   }
 }
