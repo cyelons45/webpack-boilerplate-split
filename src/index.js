12902f7d7d2a67f3db576e65b4a3cdb3670872be
Alex Thorne <alex@thorne.cc>
2017-12-10 17:49:21 +0000

    Enable HMR in development build
    Also add `npm run start` to start dev server.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // main entry point: write your JavaScript here
 console.log('Hello World!')
+
+if (module.hot) {
+  module.hot.accept()
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,15 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: common.output.path,
+    compress: true,
+    port: 3000,
+    hot: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 })
