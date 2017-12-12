096e69789c92126c0a466520e086d8b83d5a93e1
Alex Thorne <alex@thorne.cc>
2017-12-12 00:06:14 +0000

    Replace `path.resolve` with `path.join`
    They are equivalent here since `__dirname` is the *absolute* path of the
    current module directory and we are simply concatenating the relative
    path `public` to this.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, outDir)
+    path: path.join(__dirname, outDir)
   },
   module: {
     loaders: [
