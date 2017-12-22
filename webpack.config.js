39bb79ee7ff7c86b3607de62e9c737dd5ec7cc74
Alex Thorne <alex@thorne.cc>
2017-12-22 21:09:38 +0000

    Split Babel config into separate `.babelrc`.

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      },
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
       { test: /\.(jpg|png|gif|svg|tiff)$/, use: 'file-loader' },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' }
